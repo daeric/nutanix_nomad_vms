5a8d93dae724fd07b754c38744697e79972b973f
Daeric <daeric@hashicorp.com>
2022-11-29 12:59:23 -0600

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,26 @@
+
+variable "cluster_name" {
+  type = string
+}
+variable "subnet_name" {
+  type = string
+}
+variable "password" {
+  type      = string
+  sensitive = true
+}
+variable "endpoint" {
+  type = string
+}
+variable "user" {
+  type = string
+}
+variable "instance_count" {
+  type = string
+}
+data "nutanix_cluster" "cluster" {
+  name = var.cluster_name
+}
+data "nutanix_subnet" "subnet" {
+  subnet_name = var.subnet_name
+}
